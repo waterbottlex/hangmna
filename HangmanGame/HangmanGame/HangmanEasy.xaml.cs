4150b600cd8b9fd9d93a83476b7332a55a054b33
waterbottlex <38332614+waterbottlex@users.noreply.github.com>
2020-08-07 18:52:36 -0400

    Added word on UI page

--- HangmanGame/HangmanGame/HangmanEasy.xaml.cs
+++ HangmanGame/HangmanGame/HangmanEasy.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
@@ -11,10 +12,12 @@ namespace HangmanGame
     public partial class HangmanEasy : Page
     {
         public static char[] guesses = new char[26];
-        public static string[] words = { "bruh", "bruh" };
+        public static string[] words = { "india", "watermelon" };
         public static int x = RandomNumber();
         public static string chosenWord = words[x];
-        public char[] word = chosenWord.ToCharArray();
+        public static char[] word = chosenWord.ToCharArray();
+        public static char[] display = new char[(word.Length*2)];
+        public static string displayed;
 
         public HangmanEasy()
         {
@@ -25,6 +28,15 @@ namespace HangmanGame
             {
                 guesses[i] = '.';
             }
+
+            for(int i = 0; i < (word.Length*2); i+=2)
+            {
+                display[i] = '_';
+                display[i + 1] = ' ';
+            }
+
+            displayed = new string(display);
+            WordShown.Text = displayed;
         }
 
 
@@ -71,6 +83,18 @@ namespace HangmanGame
                 }
             }
 
+
+            for(int i = 0; i < display.Length; i += 2)
+            {
+                if(correctGuesses[i/2] == true)
+                {
+                    display[i] = word[i/2];
+                }
+            }
+            displayed = new string(display);
+            WordShown.Text = displayed;
+
+
             bool win = true; ;
             for(int i = 0; i < correctGuesses.Length; i++)
             {
@@ -96,7 +120,7 @@ namespace HangmanGame
             guesses[0] = 'a';
             CheckGame(guesses, word);
             AButton.Visibility = Visibility.Collapsed;
-
+            
 
         }
 
@@ -104,42 +128,49 @@ namespace HangmanGame
         {
             guesses[1] = 'b';
             CheckGame(guesses, word);
+            BButton.Visibility = Visibility.Collapsed;
 
         }
         private void cClick(object sender, RoutedEventArgs e)
         {
             guesses[2] = 'c';
             CheckGame(guesses, word);
+            CButton.Visibility = Visibility.Collapsed;
 
         }
         private void dClick(object sender, RoutedEventArgs e)
         {
             guesses[3] = 'd';
             CheckGame(guesses, word);
+            DButton.Visibility = Visibility.Collapsed;
 
         }
         private void eClick(object sender, RoutedEventArgs e)
         {
             guesses[4] = 'e';
             CheckGame(guesses, word);
+            EButton.Visibility = Visibility.Collapsed;
 
         }
         private void fClick(object sender, RoutedEventArgs e)
         {
             guesses[5] = 'f';
             CheckGame(guesses, word);
+            FButton.Visibility = Visibility.Collapsed;
 
         }
         private void gClick(object sender, RoutedEventArgs e)
         {
             guesses[6] = 'g';
             CheckGame(guesses, word);
+            GButton.Visibility = Visibility.Collapsed;
         }
 
         private void hClick(object sender, RoutedEventArgs e)
         {
             guesses[7] = 'h';
             CheckGame(guesses, word);
+            HButton.Visibility = Visibility.Collapsed;
 
         }
         private void iClick(object sender, RoutedEventArgs e)
@@ -147,54 +178,63 @@ namespace HangmanGame
             guesses[8] = 'i';
 
             CheckGame(guesses, word);
+            IButton.Visibility = Visibility.Collapsed;
         }
         private void jClick(object sender, RoutedEventArgs e)
         {
             guesses[9] = 'j';
 
             CheckGame(guesses, word);
+            JButton.Visibility = Visibility.Collapsed;
         }
 
         private void kClick(object sender, RoutedEventArgs e)
         {
             guesses[10] = 'k';
             CheckGame(guesses, word);
+            KButton.Visibility = Visibility.Collapsed;
 
         }
         private void lClick(object sender, RoutedEventArgs e)
         {
             guesses[11] = 'l';
             CheckGame(guesses, word);
+            LButton.Visibility = Visibility.Collapsed;
 
         }
         private void mClick(object sender, RoutedEventArgs e)
         {
             guesses[12] = 'm';
             CheckGame(guesses, word);
+            MButton.Visibility = Visibility.Collapsed;
 
         }
         private void nClick(object sender, RoutedEventArgs e)
         {
             guesses[13] = 'n';
             CheckGame(guesses, word);
+            NButton.Visibility = Visibility.Collapsed;
 
         }
         private void oClick(object sender, RoutedEventArgs e)
         {
             guesses[14] = 'o';
             CheckGame(guesses, word);
+            OButton.Visibility = Visibility.Collapsed;
 
         }
         private void pClick(object sender, RoutedEventArgs e)
         {
             guesses[15] = 'p';
             CheckGame(guesses, word);
+            PButton.Visibility = Visibility.Collapsed;
 
         }
         private void qClick(object sender, RoutedEventArgs e)
         {
             guesses[16] = 'q';
             CheckGame(guesses, word);
+            QButton.Visibility = Visibility.Collapsed;
 
         }
         private void rClick(object sender, RoutedEventArgs e)
@@ -202,41 +242,48 @@ namespace HangmanGame
             guesses[17] = 'r';
 
             CheckGame(guesses, word);
+            RButton.Visibility = Visibility.Collapsed;
         }
         private void sClick(object sender, RoutedEventArgs e)
         {
             guesses[18] = 's';
             CheckGame(guesses, word);
+            SButton.Visibility = Visibility.Collapsed;
 
         }
         private void tClick(object sender, RoutedEventArgs e)
         {
             guesses[19] = 't';
             CheckGame(guesses, word);
+            TButton.Visibility = Visibility.Collapsed;
 
         }
         private void uClick(object sender, RoutedEventArgs e)
         {
             guesses[20] = 'u';
             CheckGame(guesses, word);
+            UButton.Visibility = Visibility.Collapsed;
 
         }
         private void vClick(object sender, RoutedEventArgs e)
         {
             guesses[21] = 'v';
             CheckGame(guesses, word);
+            VButton.Visibility = Visibility.Collapsed;
 
         }
         private void wClick(object sender, RoutedEventArgs e)
         {
             guesses[22] = 'w';
             CheckGame(guesses, word);
+            WButton.Visibility = Visibility.Collapsed;
 
         }
         private void xClick(object sender, RoutedEventArgs e)
         {
             guesses[23] = 'x';
             CheckGame(guesses, word);
+            XButton.Visibility = Visibility.Collapsed;
 
         }
         private void yClick(object sender, RoutedEventArgs e)
@@ -244,12 +291,14 @@ namespace HangmanGame
             guesses[24] = 'y';
 
             CheckGame(guesses, word);
+            YButton.Visibility = Visibility.Collapsed;
         }
         private void zClick(object sender, RoutedEventArgs e)
         {
             guesses[25] = 'z';
 
             CheckGame(guesses, word);
+            ZButton.Visibility = Visibility.Collapsed;
         }
     }
 }
